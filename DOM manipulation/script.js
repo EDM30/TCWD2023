c4d6ffb5d2c25f947c27ec8f0d6982350e217d22
EDM30 <edavid.manaog@gmail.com>
2024-04-18 11:44:12 +1000

    Background generator for css

--- DOM manipulation/script.js
+++ DOM manipulation/script.js
@@ -2,15 +2,22 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-function inputLength() {
-	return input.value.length;
+function createListElement() {
+	var div = document.createElement("div");
+	var li = document.createElement("li");
+	var delButton = document.createElement("button");
+	div.classList.add("wrapper");
+	ul.appendChild(div);
+	div.append(li, delButton);
+	li.classList.add("taskClass");
+	li.appendChild(document.createTextNode(input.value));
+	input.value = "";
+	delButton.classList.add("delClass");
+	delButton.innerHTML='Del';
 }
 
-function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+function inputLength() {
+	return input.value.length;
 }
 
 function addListAfterClick() {
@@ -25,6 +32,23 @@ function addListAfterKeypress(event) {
 	}
 }
 
-button.addEventListener("click", addListAfterClick);
+function doneTask(task) {
+	if (task.target.tagName === "LI"){
+		task.target.classList.toggle("done");
+	}
+}
 
+function deleteListElement(element) {
+	if (element.target.className === "delClass"){
+		element.target.parentElement.remove();
+	}
+}
+
+function handleUlClick (element) {
+	doneTask(element);
+	deleteListElement(element);
+}
+
+ul.addEventListener("click", handleUlClick)
+button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
